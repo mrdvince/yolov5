a3a49f561205490fe3d23ed9f602f38daef0021b
Vince <vincivenv@gmail.com>
2021-09-02 01:53:50 +0300

    dont check for requirements

--- detect.py
+++ detect.py
@@ -278,7 +278,7 @@ def parse_opt():
 
 def main(opt):
     print(colorstr('detect: ') + ', '.join(f'{k}={v}' for k, v in vars(opt).items()))
-    check_requirements(exclude=('tensorboard', 'thop'))
+    # check_requirements(exclude=('tensorboard', 'thop'))
     run(**vars(opt))
 
 
